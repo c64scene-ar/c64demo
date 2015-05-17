a6f9c873e16e419d67f44a4ee12a17fe3349e6ec
acid <acid@shine.local>
2015-05-16 21:12:54 -0300

    music relative path

--- code/load_resources.asm
+++ code/load_resources.asm
@@ -1,5 +1,5 @@
 // load sid music
 
-.var music = LoadSid("/home/mica/Projects/c64demo/resources/jeff_donald.sid")
+.var music = LoadSid("resources/jeff_donald.sid")
 .pc = music.location
 .fill music.size, music.getData(i)
