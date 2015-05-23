39f40e9d55d3b0b6cfa9f85d5dbd607e0d3156ea
Damián Silvani <munshkr@gmail.com>
2015-05-23 15:38:27 -0300

    Small optimization on static init text
    80 cycles less (2 cycles of CMP x 40 cols).
    
    Yeah, I know. It's an init routine, so who cares.
    But I wanted to commit something :-)

--- src/init/statictext_init.asm
+++ src/init/statictext_init.asm
@@ -4,14 +4,13 @@
 
 
 init_text:
-	ldx #$00          // init X-Register with $00
+	ldx #$28          // iterate $28 times (40 cols)
 loop_text:
 	lda line1,x      // read characters from line1 table of text...
 	sta $0590,x      // ...and store in screen ram near the center
 	lda line2,x      // read characters from line1 table of text...
 	sta $05e0,x      // ...and put 2 rows below line1
 
-	inx 
-	cpx #$28         // finished when all 40 cols of a line are processed
+	dex
 	bne loop_text    // loop if we are not done yet
 	rts
