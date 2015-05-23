90a4bfa171b4779067c734068e14e3c15b540a93
Damián Silvani <munshkr@gmail.com>
2015-05-23 15:36:21 -0300

    Fix last position offset on colorwasher fx

--- src/fx/colorwash_sub.asm
+++ src/fx/colorwash_sub.asm
@@ -4,7 +4,7 @@
 
 colwash:
     lda color+$00      // load the current first color from table
-    sta color+$28      // store in in last position of table to reset the cycle
+    sta color+$27      // store in in last position of table to reset the cycle
     ldx #$00           // init X with zero
 
 cycle1:
@@ -16,10 +16,10 @@ cycle1:
     bne cycle1         // if no, continue
 
 colwash2:
-    lda color2+$28     // load current last color from second table
+    lda color2+$27     // load current last color from second table
     sta color2+$00     // store in in first position of table to reset the cycle
     ldx #$28
-    
+
 cycle2:
     lda color2-1,x     // Start cycle by fetching previous color in the table...
     sta color2,x       // ...and store it in the current active position.
