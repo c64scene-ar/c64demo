47d41103c6316e77fa2a41b86e51cca1a551ad7a
fmuttis <fmuttis@cubicalabs.com>
2015-06-07 22:31:17 -0700

    por lo menos se ve lindo y

--- vendor/spindle-1.0/nuevo/mtx.py
+++ vendor/spindle-1.0/nuevo/mtx.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from math import *
 import time
 import struct
@@ -22,11 +23,14 @@ c64p = { 0: (Fore.BLACK, Style.DIM),
         15: (Fore.WHITE, Style.NORMAL) }
 
 def show_mtx(m):
-  os.system("clear")
+  #os.system("clear")
+  sys.stderr.write("\x1b[2J\x1b[H")
+
   for y in range(25):
     for x in range(40):
       cols = c64p[m[x][y]]
-      print cols[0] + cols[1] + str(m[x][y]).zfill(2),
+      sys.stdout.write( cols[0] + cols[1] + chr(219).decode('cp437')) #str(m[x][y]).zfill(2),
+      sys.stdout.write( cols[0] + cols[1] + chr(219).decode('cp437')) #str(m[x][y]).zfill(2),
     print ""
 
 def do_circle(m, xc, yc, r):
@@ -45,12 +49,12 @@ def cmp_mtx(m1, m2):
   return changes
 
 def apply_palette(m):
-  p = [1,10,4,8,15,12,11,8,0,0,0,0,0,0,0,0]
+  p = [1,1,15,15,10,10,12,12,4,4,4,4,11,11,0,0]
   for y in range(25):
     for x in range(40):
       #m[x][y] = m[x][y] if m[x][y] < 16 else 0 
       #m[x][y] = m[x][y] if m[x][y] < 16 else 0 
-      m[x][y] = p[m[x][y] % 16] 
+      m[x][y] = p[m[x][y]] if m[x][y] < 16 else p[16 - m[x][y]] 
 
 def serialize(changes):
   ret = struct.pack("<H", len(changes))
@@ -65,20 +69,20 @@ full_changes = ""
 for i in range(0, 200):
   m = [[15 for y in range(25)] for x in range(40)] 
   #do_circle(m, cos(i/10.0)*10+20, sqrt(i/10.0)*10+10, abs(tan(i/50.0)) * 10)
-  #do_circle(m, cos(i/10.0)*10+20, sin(i/10.0)*10+10, abs(tan(i/10.0)) * 10)
+  do_circle(m, cos(i/10.0)*10+20, sin(i/10.0)*10+10, abs(tan(i/10.0)) * 10)
   #do_circle(m, cos(i/10.0)*10+20, sin(i/10.0)*10+10, abs(atan((i-10)/50.0)) * 10)
   do_circle(m, sin(i/10.0)*10+20, cos(i/10.0)*10+10, abs(atan(i/50.0)) * 10)
-  do_circle(m, cos(i/10.0)*10+20, sin(i/10.0)*10+10, abs(cos(i/50.0)) * 10)
+  #do_circle(m, cos(i/10.0)*10+20, sin(i/10.0)*10+10, abs(cos(i/50.0)) * 10)
   apply_palette(m)
   if i % 4 == 0:
     show_mtx(m)
     changes = cmp_mtx(prev_m, m)
     full_changes += serialize(changes)
-    time.sleep(0.3)
+    time.sleep(0.1)
   prev_m = m
 
 
-print "Effect table len: ", len(full_changes)
+print Fore.WHITE + Style.NORMAL + "Effect table len: ", len(full_changes)
 #print  full_changes.encode("hex")
 
 
