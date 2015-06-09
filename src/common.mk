3c33ebf78b3f75c28183ebb350e6874af8d7fdd7
Damián Silvani <munshkr@gmail.com>
2015-06-09 17:05:53 -0300

    Major refactor for Spindle
    * Rename "scenes" to "part". Spindle names his effects/scenes as "demo
      parts", we should use the same nomenclature to avoid confusion.
    
    * "music" is a demo part, so move music/ to part/.  It's not a "scene"
      either, that's another reason it's called "part" and not "effect" or
      "scene" :-)
    
    * There is a Makefile on part/ that builds or cleans all demo parts.
    
    * Each part has a small makefile that only declares its name, extra
      data prgs to be packaged on its .pef and other custom stuff that
      needs building.
    
    * Added "make run" on both src/ and each part directory.  Fires up
      Vice emulator by default.

---
+++ src/common.mk
@@ -0,0 +1,2 @@
+SPINDLE_DIR=../vendor/spindle-1.0/spindle
+PEFCHAIN=$(SPINDLE_DIR)/pefchain
---
+++ src/part/common.mk
@@ -0,0 +1,34 @@
+SPINDLE_DIR=../../../vendor/spindle-1.0/spindle
+MKPEF=$(SPINDLE_DIR)/mkpef
+PEF2PRG=$(SPINDLE_DIR)/pef2prg
+XA=xa
+
+# Variables to be defined on each part Makefile:
+#
+# Mandatory:
+#   - PART: name of part
+# Optional:
+#   - SRC: list of source files to be compiled with xa
+#   - OBJ: list of object files to be compiled into a "part effect" (.pef)
+
+SRC=$(PART).asm
+OBJ=$(PART).efo
+
+all: $(PART).pef $(PART).prg
+
+$(PART).efo: $(SRC)
+	$(XA) -o $@ $<
+
+$(PART).pef: $(OBJ)
+	${MKPEF} -o $@ $^
+
+%.prg: %.pef
+	${PEF2PRG} -o $@ $^
+
+clean:
+	rm -f *.efo *.pef *.prg
+
+run: $(PART).prg
+	x64 $<
+
+.PHONY: clean run
